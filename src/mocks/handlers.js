1a41356392c249aeb062afd5bd28bc6f8407861c
2yunseong <dbsdltjd3701@naver.com>
2022-12-19 21:44:19 +0900

    feat(msw): 요청 핸들러 작성

---
+++ src/mocks/handlers.js
@@ -0,0 +1,32 @@
+import { rest } from 'msw';
+
+const date = new Date().toLocaleString();
+const recentPosts = [
+  {
+    id: 1,
+    timeStamp: date,
+    title: '나의 React 도전기',
+    content:
+      '안녕하세요. 윤성입니다. 지난 1년간 공부했던 리액트를 다시 보는 중입니다.',
+  },
+  {
+    id: 2,
+    timeStamp: date,
+    title: '우아한 테크코스 5기 최종 코딩테스트 후기',
+    content:
+      '지난 17일 우아한 형제들에서 주관하는 우아한 테크코스 5기 FE 최종 코딩테스트가 한국 루터회관 14층에서 진행되었습니다.',
+  },
+  {
+    id: 3,
+    timeStamp: date,
+    title: '뻘글 입니다.',
+    content: '진짜 개 벌글입니다.',
+  },
+];
+
+export const handlers = [
+  // 최신 글 가져오기
+  rest.get('/posts/recent', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(recentPosts));
+  }),
+];
