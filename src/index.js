fd0a82b6768b1c70ea9843bd1cb73695a1bc1e36
2yunseong <dbsdltjd3701@naver.com>
2022-12-19 21:44:30 +0900

    feat(msw): 서비스 워커 생성 및 호출

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import { worker } from './mocks/worker';
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
