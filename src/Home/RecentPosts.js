2398527ad028c1f35d5ab50eaf3de1b221437265
2yunseong <dbsdltjd3701@naver.com>
2022-12-20 17:00:03 +0900

    feat(recentpost): 최신 게시글 목록 마크업

---
+++ src/Home/RecentPosts.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import Post from './Post';
+
+const RecentPostContainer = styled.div``;
+
+const RecentPosts = () => {
+  const [recentPosts, setRecentPosts] = useState([]);
+  useEffect(() => {
+    axios.get('/posts/recent').then((res) => {
+      setRecentPosts(() => res.data);
+    });
+  }, []);
+  return (
+    <RecentPostContainer>
+      {recentPosts.map((recentPost) => (
+        <Post
+          title={recentPost.title}
+          content={recentPost.content}
+          key={recentPost.id}
+        />
+      ))}
+    </RecentPostContainer>
+  );
+};
+
+export default RecentPosts;
