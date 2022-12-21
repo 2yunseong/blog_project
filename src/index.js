4a6b6336cfaac1a9e5bac7f00f25c928c04743fa
2yunseong <dbsdltjd3701@naver.com>
2022-12-22 01:05:04 +0900

    fix(index): body margin 제거

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
 import { worker } from './mocks/worker';
+
+import './index.scss';
+
 if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
