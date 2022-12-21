3e57aab0ac27a40fbb00b8ee09af72403992bb10
2yunseong <dbsdltjd3701@naver.com>
2022-12-21 21:25:25 +0900

    feat(portfolio): 포트폴리오 페이지 마크업

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,5 +1,28 @@
 const Portfolio = () => {
-  return <div>포트폴리오</div>;
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <h3>기술 스택</h3>
+      <ul>
+        <li>react</li>
+        <li>javascript</li>
+        <li>swift</li>
+        <li>java</li>
+      </ul>
+      <h3>대외활동 및 수상내역</h3>
+      <ul>
+        <li>전남대학교 SW 프로그래밍 경진대회 전공부 장려상</li>
+        <li>SW중심대학 공통해커톤 2022 후원기업상(OSBC)상</li>
+        <li>전남대학교 IT개발동아리 에코노베이션 회장(2022~ )</li>
+        <li>22' Naver D2 Campus Partner 활동 중</li>
+        <li>2022 오픈소스 컨트리뷰션 아카데미 Master's 멘티 수료</li>
+      </ul>
+      <h3>프로젝트 소개</h3>
+      <h4>에코노 냉장고</h4>
+      <h4>EggMeet</h4>
+      <h4>T-econo</h4>
+    </div>
+  );
 };
 
 export default Portfolio;
