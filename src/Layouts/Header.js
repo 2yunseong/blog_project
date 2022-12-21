d6ea2157a97c9e86e07e15b75d7d5224479ba35a
2yunseong <dbsdltjd3701@naver.com>
2022-12-22 01:18:42 +0900

    feat(header): header css 구현

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,15 +1,53 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import './Header.module.scss';
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ddd;
+  padding: 10px 30px;
+`;
 
+const handleNavStyle = ({ isActive }) => {
+  return {
+    color: 'black',
+    textDecoration: 'none',
+    outline: 'none',
+    borderBottom: isActive ? '2px solid black' : '',
+    margin: '30px 30px 0 30px',
+  };
+};
+
+const handleTitleStyle = ({ isActive }) => {
+  return {
+    color: 'black',
+    textDecoration: 'none',
+    outline: 'none',
+    fontSize: '1.5em',
+    fontWeight: '700',
+  };
+};
+
+const NavContainer = styled.div``;
 const Header = () => {
   return (
     <HeaderContainer>
-      <NavLink to='/'>Yunseong.Devlog</NavLink>
-      <NavLink to='/'>Profile</NavLink>
-      <NavLink to='/portfolio'>Poftfolio</NavLink>
-      <NavLink to='/posts'>Post</NavLink>
+      <NavLink style={handleTitleStyle} to='/' className='title'>
+        Yunseong.Devlog
+      </NavLink>
+      <NavContainer>
+        <NavLink style={handleNavStyle} to='/'>
+          Profile
+        </NavLink>
+        <NavLink style={handleNavStyle} to='/portfolio'>
+          Poftfolio
+        </NavLink>
+        <NavLink style={handleNavStyle} to='/posts'>
+          Post
+        </NavLink>
+      </NavContainer>
     </HeaderContainer>
   );
 };
