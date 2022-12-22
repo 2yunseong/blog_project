26b0f3e520e6b1d0e6c194a8269f00d736e36f0f
2yunseong <dbsdltjd3701@naver.com>
2022-12-22 20:52:51 +0900

    feat(post): 게시물 개별 컴포넌트

---
+++ src/Post/Post.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import styled from 'styled-components';
+import MainContainer from '../styles/MainContainer';
+
+const PostContainer = styled(MainContainer)``;
+const Title = styled.h1``;
+
+const Post = () => {
+  <PostContainer>
+    <Title>{}</Title>
+  </PostContainer>;
+};
+
+export default Post;
