76eac11bf2159c09419ae757c4daaf8a7b98034d
2yunseong <dbsdltjd3701@naver.com>
2022-12-22 12:55:55 +0900

    feat(write): 글쓰기 컴포넌트 css 작업

--- src/Write/Write.js
+++ src/Write/Write.js
@@ -6,13 +6,61 @@ import MainContainer from '../styles/MainContainer';
 
 const WriteContainer = styled(MainContainer)``;
 
-const SubmitBtn = styled.button``;
+const SubmitBtn = styled.button`
+  background-color: white;
+  outline: none;
+  border: 1px solid #dadce5;
+  width: 12rem;
+  height: 3.5rem;
+  font-size: 1.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    border: 1px solid white;
+    color: white;
+    background-color: #dadce5;
+  }
+`;
+
+const TitleInput = styled.input`
+  flex: 1;
+  font-size: 1.2rem;
+  padding: 0;
+  margin: 0;
+  border: 1px solid #dadce5;
+  border-radius: 5px;
+  padding-left: 10px;
+`;
+
+const TitleLabel = styled.span`
+  margin-right: 0.8rem;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  font-size: 1.2rem;
+  line-height: 2;
+  margin: 1.5rem 0;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  margin: 1rem 0;
+`;
+
 const Write = () => {
   return (
     <WriteContainer>
       <h1>글 쓰기</h1>
-      <Editor usageStatistics={false} />
-      <SubmitBtn>제출</SubmitBtn>
+      <TitleContainer>
+        <TitleLabel>제목 :</TitleLabel>
+        <TitleInput placeholder='제목을 입력하세요.' />
+      </TitleContainer>
+      <Editor height='30rem' usageStatistics={false} />
+      <Footer>
+        <SubmitBtn>제출</SubmitBtn>
+      </Footer>
     </WriteContainer>
   );
 };
