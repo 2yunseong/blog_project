388003ae8e79f2ef9a2eb01838bded653d354688
2yunseong <dbsdltjd3701@naver.com>
2022-12-23 01:18:15 +0900

    feat(msw): 글 게시 로직 모킹
    - POST : /post

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,11 @@
 import { rest } from 'msw';
-import recentPosts from './data/recentPosts';
 import profiles from './data/profiles';
 import posts from './data/posts';
 
 export const handlers = [
   // 최신 글 가져오기
   rest.get('/posts/recent', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(recentPosts));
+    return res(ctx.status(200), ctx.json(posts));
   }),
   // user 프로필 가져오기
   rest.get('/user/profile', (req, res, ctx) => {
@@ -24,9 +23,17 @@ export const handlers = [
     return res(ctx.status(200), ctx.json(post));
   }),
 
+  rest.post('/post', (req, res, ctx) => {
+    const resDto = {
+      id: posts.length + 1,
+      ...req.body,
+    };
+    console.log(resDto);
+    posts.push(resDto);
+    return res(ctx.status(200));
+  }),
+
   rest.get('/comment', (req, res, ctx) => {}),
 
   rest.post('/comment', (req, res, ctx) => {}),
-
-  rest.post('/post', (req, res, ctx) => {}),
 ];
