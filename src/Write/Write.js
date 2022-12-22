fc93c3d20b4ae27a5746b26172feeb3e990a8cde
2yunseong <dbsdltjd3701@naver.com>
2022-12-22 12:27:48 +0900

    feat(write): 글쓰기 파트 마크업

--- src/Write/Write.js
+++ src/Write/Write.js
@@ -1,7 +1,20 @@
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import { Editor } from '@toast-ui/react-editor';
+import styled from 'styled-components';
+import MainContainer from '../styles/MainContainer';
 
-const Write = () => {};
+const WriteContainer = styled(MainContainer)``;
+
+const SubmitBtn = styled.button``;
+const Write = () => {
+  return (
+    <WriteContainer>
+      <h1>글 쓰기</h1>
+      <Editor usageStatistics={false} />
+      <SubmitBtn>제출</SubmitBtn>
+    </WriteContainer>
+  );
+};
 
 export default Write;
